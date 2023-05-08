22821be5c59f49d84647f9ca1027dac0a97adc0d
mamadinit <mohammad.s.jamal13@gmail.com>
2023-05-09 00:27:49 +0330

    views for ProductDetailView

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from typing import Any, Optional
+from django.db import models
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+from django.views.generic import DetailView
 
 # Create your views here.
+
+
+
+class ProductDetailView(DetailView):
+    def get_object(self):
+        slug = self.kwargs.get('slug')
+        product = get_object_or_404(Product, slug=slug)
+
+        return product
