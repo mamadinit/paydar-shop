ccb87046263e4612c8c0be0b9945b8001fc4a903
mamadinit <mohammad.s.jamal13@gmail.com>
2023-05-09 00:29:55 +0330

    delete some url config

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from .views import SignUpView, VerifyView, CustomLoginView, ResendVerifyView
 from .forms import CustomPasswordResetForm
@@ -14,12 +12,3 @@ urlpatterns = [
     path('verify/resend/', ResendVerifyView.as_view(), name='resend'),
 ]
 
-
-
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
