6622e55bcffc58c064b54e273513a352ba41871b
mamadinit <mohammad.s.jamal13@gmail.com>
2023-05-09 00:28:36 +0330

    add ProductAdmin, CategoryAdmin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Product, Picture, Category, Classification, Attribute
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug":("title",)}
+
+admin.site.register(Product, ProductAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Category, CategoryAdmin)
+
+admin.site.register(Picture)
+
+admin.site.register(Classification)
+admin.site.register(Attribute)
