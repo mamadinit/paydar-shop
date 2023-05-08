2303aadf4a6e4d4c612fed8c69d986968947e2b7
mamadinit <mohammad.s.jamal13@gmail.com>
2023-05-08 21:04:22 +0330

    register new app (widget_tweaks)

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'account.apps.AccountConfig',
     'shop.apps.ShopConfig',
     'blog.apps.BlogConfig',
+
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -125,8 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [path.join(BASE_DIR, 'static')]
+# STATIC_ROOT = path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = path.join(BASE_DIR, 'media')
