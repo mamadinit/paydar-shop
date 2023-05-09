bfc9f6afa975541685d9617813a37c0b4a02335e
mamadinit <mohammad.s.jamal13@gmail.com>
2023-05-09 11:57:46 +0330

    add cart urls

--- shop/urls.py
+++ shop/urls.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from .views import ProductDetailView
 
-app_name = 'account'
+from .views import CartAddView, CartDetailView
+
+app_name = 'shop'
 
 
 urlpatterns = [
     path('product/<slug:slug>/', ProductDetailView.as_view(), name='product-detail'),
+    path('cart/add/<product_id>/', CartAddView.as_view(), name='cart_add'),
+    path('cart/', CartDetailView.as_view(), name='cart-detail'),
 ]
 
 
