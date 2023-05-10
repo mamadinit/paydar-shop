d043cb99bbccbd36f613cc4ea93fef5f524ce42b
mamadinit <mohammad.s.jamal13@gmail.com>
2023-05-10 19:46:29 +0330

    add CouponForm

--- shop/forms.py
+++ shop/forms.py
@@ -4,3 +4,7 @@ from django import forms
 class Add2CartForm(forms.Form):
     quantity = forms.IntegerField(min_value=1, max_value=9, widget=forms.NumberInput(attrs={'class': 'form-control',
                                                                                             'placeholder': 'quantity'}))
+
+
+class CouponForm(forms.Form):
+    code = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':' کد تخفیف '}), label='')
