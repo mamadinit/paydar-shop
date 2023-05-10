b80a46cf92ef1f2afb6b57b30c444f7e62bcebe0
mamadinit <mohammad.s.jamal13@gmail.com>
2023-05-10 15:37:09 +0330

    add new url for delete item cart

--- shop/urls.py
+++ shop/urls.py
@@ -2,15 +2,16 @@ from django.contrib import admin
 from django.urls import path
 from .views import ProductDetailView
 
-from .views import CartAddView, CartDetailView
+from .views import CartAddItemView, CartDetailView, CartRemoveItemView
 
 app_name = 'shop'
 
 
 urlpatterns = [
     path('product/<slug:slug>/', ProductDetailView.as_view(), name='product-detail'),
-    path('cart/add/<product_id>/', CartAddView.as_view(), name='cart_add'),
     path('cart/', CartDetailView.as_view(), name='cart-detail'),
+    path('cart/add/<product_id>/', CartAddItemView.as_view(), name='cart_add'),
+    path('cart/remove/<int:product_id>/', CartRemoveItemView.as_view(), name='cart_remove'),
 ]
 
 
