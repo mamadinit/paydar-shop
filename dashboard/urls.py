fcf958b4557db49cffbf647de00e8f0bf6818a7c
mamadinit <mohammad.s.jamal13@gmail.com>
2023-05-12 22:29:00 +0330

    add new url pattern (dashboard-comments)

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import DashboardInfoView, AddressUpdateView
+from .views import DashboardInfoView, DashboardAddressView, DashboardOrderView, DashboardCommentView
 
 
 app_name = 'dashboard'
@@ -9,6 +9,8 @@ app_name = 'dashboard'
 
 urlpatterns = [
     path('', DashboardInfoView.as_view(), name='dashboard-info'),
-    path('address/', AddressUpdateView.as_view(), name='dashboard-address'),
+    path('address/', DashboardAddressView.as_view(), name='dashboard-address'),
+    path('oreders/', DashboardOrderView.as_view(), name='dashboard-order'),
+    path('comments/', DashboardCommentView.as_view(), name='dashboard-comments'),
 ]
 
