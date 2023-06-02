af972a454efe5b1952812e074ee6cf2f356157d6
mamadinit <mohammad.s.jamal13@gmail.com>
2023-06-02 16:03:30 +0330

    articledetial viwes

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
+from typing import Any, Optional
+from django.db import models
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Article
 
 # Create your views here.
+
+class ArticleDetailView(DetailView):
+
+    def get_object(self):
+        slug = self.kwargs.get('slug')
+
+
+
+
+
+
+
