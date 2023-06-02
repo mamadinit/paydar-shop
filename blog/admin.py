f7708ed916193417d1a8dff911ceb523fe29340b
mamadinit <mohammad.s.jamal13@gmail.com>
2023-06-02 16:03:51 +0330

    add admin page article

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Article, Category
 
 # Register your models here.
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["title", "slug", "status"]
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Article, ArticleAdmin)
+
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["title", "slug", "position"]
+    list_editable = ["position"]
+
+admin.site.register(Category, CategoryAdmin)
